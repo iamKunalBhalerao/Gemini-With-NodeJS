397153b40b60bb7b4c1711ab3477cf6df8d852a5
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-21 19:45:20 +0530

    Created simple AI Integrated chat bot in Node js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI("GEMINI_API_KEY");
+require("dotenv").config();
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain how AI works";
+const prompt =
+  "Give me the specific diet plan for me, my weight is 87kg, i am 19 years old";
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+const geminiResponse = async () => {
+  try {
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+geminiResponse();
