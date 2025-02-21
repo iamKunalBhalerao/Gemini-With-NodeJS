139b47166917c4f14e82fee375500f9d9cd13d23
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-21 20:01:45 +0530

    Created a Specific Route for geting a ans from gemini

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+const express = require("express");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt =
-  "Give me the specific diet plan for me, my weight is 87kg, i am 19 years old";
+const app = express();
+app.use(express.json());
 
-const geminiResponse = async () => {
+// const prompt =
+//   "Give me the specific diet plan for me, my weight is 87kg, i am 19 years old";
+
+app.post("/getans", async (req, res) => {
+  const prompt = req.body.prompt;
+
   try {
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-geminiResponse();
+    res.status(200).json({
+      Answer: result.response.text(),
+    });
+  } catch (error) {
+    res.json({
+      Error: error,
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server is on PORT:3000");
+});
